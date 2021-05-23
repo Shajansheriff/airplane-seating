381bb032d9930d94d80bce184507e3733fe68707
Shajahan <shajansheriff@gmail.com>
2021-05-23 17:56:24 +0530

    test(airplane): update the spec with given example scenario

--- src/services/airplane/airplane.spec.js
+++ src/services/airplane/airplane.spec.js
@@ -73,4 +73,74 @@ describe('Create seating chart', () => {
       ],
     ]);
   });
+
+  it('should return an 3x2, 4x3, 2x3, 3x4 seating chart', () => {
+    const chart = Airplane.createSeatingChart([
+      [3, 2],
+      [4, 3],
+      [2, 3],
+      [3, 4],
+    ]);
+    expect(chart).toEqual([
+      [
+        [WindowSeatConstructor(0), AisleSeatConstructor(1)],
+        [WindowSeatConstructor(1), AisleSeatConstructor(2)],
+        [WindowSeatConstructor(2), AisleSeatConstructor(3)],
+      ],
+      [
+        [
+          AisleSeatConstructor(0),
+          MiddleSeatConstructor(1),
+          AisleSeatConstructor(2),
+        ],
+        [
+          AisleSeatConstructor(1),
+          MiddleSeatConstructor(2),
+          AisleSeatConstructor(3),
+        ],
+        [
+          AisleSeatConstructor(2),
+          MiddleSeatConstructor(3),
+          AisleSeatConstructor(4),
+        ],
+        [
+          AisleSeatConstructor(3),
+          MiddleSeatConstructor(4),
+          AisleSeatConstructor(5),
+        ],
+      ],
+      [
+        [
+          AisleSeatConstructor(0),
+          MiddleSeatConstructor(1),
+          AisleSeatConstructor(2),
+        ],
+        [
+          AisleSeatConstructor(1),
+          MiddleSeatConstructor(2),
+          AisleSeatConstructor(3),
+        ],
+      ],
+      [
+        [
+          AisleSeatConstructor(0),
+          MiddleSeatConstructor(1),
+          MiddleSeatConstructor(2),
+          WindowSeatConstructor(3),
+        ],
+        [
+          AisleSeatConstructor(1),
+          MiddleSeatConstructor(2),
+          MiddleSeatConstructor(3),
+          WindowSeatConstructor(4),
+        ],
+        [
+          AisleSeatConstructor(2),
+          MiddleSeatConstructor(3),
+          MiddleSeatConstructor(4),
+          WindowSeatConstructor(5),
+        ],
+      ],
+    ]);
+  });
 });
