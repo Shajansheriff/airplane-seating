744d700072d4b72750c89de7d3a18baae0f0b7ef
Shajahan <shajansheriff@gmail.com>
2021-05-23 16:01:22 +0530

    chore(lint): update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:jest/recommended',
     'plugin:prettier/recommended'
   ],
-  plugins: ['react', '@typescript-eslint', 'jest'],
+  plugins: ['react', '@typescript-eslint', 'jest', 'prettier'],
   env: {
     browser: true,
     es6: true,
@@ -31,6 +31,10 @@ module.exports = {
       'error',
       {
         endOfLine: 'auto',
+        trailingComma: 'all',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
       },
     ],
   },
